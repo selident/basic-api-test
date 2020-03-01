ef8292b055729c383bd226426a940dec1e5ae31d
Long Lee <longhoangly@gmail.com>
2020-03-01 12:54:00 +0700

    - Add MainClass to run all test by using fat-test jar file - Edit log to debug to show more info - can disable on prod! - Edit github action step names

---
+++ src/test/java/testng/MainClass.java
@@ -0,0 +1,35 @@
+package testng;
+
+import org.testng.TestNG;
+import org.testng.collections.Lists;
+
+import java.io.*;
+import java.util.List;
+
+public class MainClass {
+
+    public static void main(String[] args) throws IOException {
+
+        String xmlFile = "testng.xml";
+
+        // Get the content of testng.xml
+        ClassLoader classLoader = MainClass.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(xmlFile);
+
+        assert inputStream != null;
+        byte[] buffer = new byte[inputStream.available()];
+        inputStream.read(buffer);
+
+        File targetFile = new File(xmlFile);
+        try (OutputStream outStream = new FileOutputStream(targetFile)) {
+            outStream.write(buffer);
+
+            // Run all the tests in testng.xml
+            TestNG testng = new TestNG();
+            List<String> suites = Lists.newArrayList();
+            suites.add(xmlFile);
+            testng.setTestSuites(suites);
+            testng.run();
+        }
+    }
+}
